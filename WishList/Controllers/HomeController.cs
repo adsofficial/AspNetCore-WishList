cbbd42ac70d16b4fabea2516e00f8a8f7e940621
Amandeepsingh <adsofficial@hotmail.com>
2022-02-26 16:59:16 +0530

    changes

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index.cshtml");
         }
 
         public IActionResult Error()
         {
-            return View();
+            return View("Error.cshtml");
         }
     }
 }
